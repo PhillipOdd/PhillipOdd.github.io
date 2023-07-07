054ac77165b701e353b60452dfe74fd9a7a8f0e5
Phillip Lloyd <phillip.lloyd@student.moringaschool.com>
2023-07-07 08:06:56 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function demoChain(name) {
+    const part1 = "hi";
+    return function () {
+        const part2 = "there";
+        return function () {
+            console.log(`${part1.toUpperCase()} ${part2} ${name}`);
+        };
+    };
+}
+
+demoChain("Dr. Stephen Strange")()(); //=> HI there Dr. Stephen Strange
